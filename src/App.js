ad6a1203e631d07666ddaf7b030191409c9f5c82
Olabanji121 <olabanjiabimbola@gmail.com>
2019-10-30 14:45:34 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import React, { Fragment} from "react";
+import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import NavBar from "./Components/layout/NavBar";
-import Users from "./Components/users/Users";
 import User from "./Components/users/User";
-import Search from "./Components/users/Search";
 import Alert from "./Components/layout/Alert";
+import Home from "./Components/pages/Home";
 import About from "./Components/pages/About";
+import NotFound from "./Components/pages/NotFound";
 import GithubState from "./context/github/GithubState";
 import AlertState from "./context/Alert/AlertState";
 
 const App = () => {
-		
 	return (
 		<GithubState>
 			<AlertState>
@@ -19,20 +18,12 @@ const App = () => {
 					<div className="App">
 						<NavBar title="Github Finder" icon="fab fa-github" />
 						<div className="container">
-							<Alert/>
+							<Alert />
 							<Switch>
-								<Route
-									exact
-									path="/"
-									render={props => (
-										<Fragment>
-											<Search/>
-											<Users />
-										</Fragment>
-									)}
-								/>
+								<Route exact path="/" component={Home} />
 								<Route exact path="/about" component={About} />
 								<Route exact path="/User/:login" component={User} />
+								<Route component={NotFound}/>
 							</Switch>
 						</div>
 					</div>
